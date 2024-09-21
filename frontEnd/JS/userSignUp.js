4a92d841e1e348959cfcd0ab721445bce474bc14
Griffin <griffin.melchoir13@gmail.com>
2024-09-21 18:57:56 +0800

    added the html and css for the sign up

--- frontEnd/JS/userSignUp.js
+++ frontEnd/JS/userSignUp.js
@@ -1,4 +1,4 @@
-localStorage.clear()
+// localStorage.clear()
 if(localStorage.getItem('userInfo') === null){
   const userPage = document.querySelector('.userStats')
   userPage.style.display = 'none'
@@ -6,3 +6,31 @@ if(localStorage.getItem('userInfo') === null){
   const userSignUp = document.querySelector('.signUp')
   userSignUp.style.display = 'none'
 }
+
+const signUpForm = document.querySelector('.signForm')
+signUpForm.addEventListener('submit', async (e)=>{
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+  try {
+    fetch('/userProfile', { 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    .then(res => res.json())
+    .then(
+
+    )
+  } catch (error) {
+    window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+  }
+
+})
+
+
+
+
+  
